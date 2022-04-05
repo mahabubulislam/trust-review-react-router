ceb5e619bb95d2b9a42d1bc718f835636901cec8
“mahbub-webdev” <mahabub2k1@gmail.com>
2022-04-05 06:04:17 +0600

    handled see all review button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,9 @@ import Review from '../Reviews/Reviews.js';
 const Home = () => {
     return (
         <>
-            <div className='flex justify-between items-center w-3/4 mx-auto my-24'>
-                <div className='w-3/5'>
-                    <h1 className='text-4xl font-bold'>
+            <div className='flex flex-col md:flex-row-reverse justify-between items-center w-auto md:w-3/4 mx-auto my-10'>
+                <div className='w-3/5 order-2'>
+                    <h1 className='text-xl md:text-4xl font-bold'>
                         AirPods Max — a perfect balance of exhilarating high-fidelity audio and the effortless magic of AirPods.
                     </h1>
                     <p>
@@ -16,7 +16,7 @@ const Home = () => {
                     <button className='rounded-full text-2xl bg-emerald-500 p-2 my-3 outline-1 shadow-lg shadow-emerald-500 hover:shadow-rose-500/50 hover:bg-rose-500 hover:scale-105'>Specifications</button>
                 </div>
                 <div>
-                    <img className='w-36 md:w-80 lg:w-96' src="https://www.apple.com/v/airpods-max/d/images/overview/hero__gnfk5g59t0qe_large.png" alt="apple headphone" />
+                    <img className='w-36 md:w-80 lg:w-96 order-1' src="https://www.apple.com/v/airpods-max/d/images/overview/hero__gnfk5g59t0qe_large.png" alt="apple headphone" />
                 </div>
             </div>
             <Reviews></Reviews>
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useReviews from '../../hook/useReviews';
 import Review from '../Review';
 
 const Reviews = () => {
     const [reviews, setReviews] = useReviews()
+    const navigate = useNavigate()
     return (
-        <div className='w-4/5 mx-auto'>
+        <div className='w-4/5 mx-auto my-5'>
             <h1 className='text-5xl font-bold text-center'>Customer Reviews (3)</h1>
-           <div className='grid grid-cols-1 gap-5 md:grid-cols-3 my-2'>
-               {
-                   reviews.slice(0,3).map(review => <Review key={review.id} review={review}></Review>)
-               }
-           </div>
+            <div className='grid grid-cols-1 gap-5 md:grid-cols-3 my-2'>
+                {
+                    reviews.slice(0, 3).map(review => <Review key={review.id} review={review}></Review>)
+                }
+            </div>
+            <div className='mx-auto w-1/5'>
+                <button onClick={()=>navigate('/reviews')} className='rounded-full text-2xl bg-emerald-500 p-2 my-3 outline-1 shadow-lg shadow-emerald-500 hover:shadow-rose-500/50 hover:bg-rose-500 hover:scale-105'>See All Reviews</button>
+            </div>
         </div>
     );
 };

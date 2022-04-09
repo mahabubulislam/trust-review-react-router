773f64652d8923c58e7cd455eab4c037600281eb
mahbub-webdev <mahabub2k1@gmail.com>
2022-04-09 21:34:24 +0600

    nav responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import { MenuIcon, XIcon } from '@heroicons/react/solid'
 const Header = () => {
     const [open, setOpen] = useState(false);
     return (
-        <div className='shadow-md w-full fixed md:relative top-0 left-0 '>
+        <div className='shadow-md w-full fixed top-0 left-0 z-[900]'>
             <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
                 <div className='font-bold text-2xl cursor-pointer flex items-center text-gray-800'>
                     <span className='text-3xl font-bold mr-1 pt-2'>
